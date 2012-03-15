b9ccaac4372cf82f3237b9bdc1898328d0d1912d
Linn Vikre <vikre.90@gmail.com>
2012-03-15 12:33:25 +0100

    Database

--- src/gruppe35/db/Database.java
+++ src/gruppe35/db/Database.java
@@ -1,5 +1,12 @@
 package gruppe35.db;
 
 public class Database {
-
+	private String driver = "";
+	private String host = "jdbc:mysql://mysql.stud.ntnu.no/____";
+	private String user = "";
+	private String pass = "";
+	//private Connection connect;
+	
+	
+	
 }
